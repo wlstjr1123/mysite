33ce508b0086d268d8b0707630d3087f9f709c41
wlstjr1123 <wlstjr1123@naver.com>
2021-10-12 17:23:19 +0900

    user service #5

--- mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.douzone.mysite.dao.UserDao;
 import com.douzone.mysite.vo.UserVo;
@@ -27,7 +28,10 @@ public class LoginAction implements Action {
 		}
 		
 		/* 인증 처리(세션처리) */
-		System.out.println("인증 처리(세션처리)");
+		HttpSession session = request.getSession(true);
+		session.setAttribute("authUser", vo);
+		
+		MvcUtil.redirect("/mysite02", request, response);
 	}
 
 }
--- mysite02/src/main/java/com/douzone/mysite/mvc/user/UserActionFactory.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/user/UserActionFactory.java
@@ -20,6 +20,8 @@ public class UserActionFactory extends ActionFactory {
 			action = new LoginFormAction();
 		} else if ("login".equals(actionName)) {
 			action = new LoginAction();
+		} else if ("logout".equals(actionName)) {
+			action = new LogoutAction();
 		}
 		else {
 			action = new MainAction();
